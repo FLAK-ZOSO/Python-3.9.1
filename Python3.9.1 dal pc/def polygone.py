ee18793a62544619640958837e4a1e68508aa148
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2021-04-25 09:45:16 +0200

    Delete Python3.9.1 dal pc directory

--- Python3.9.1 dal pc/def polygone.py
+++
@@ -1,8 +0,0 @@
-def Polygone(sides, long):
-    from turtle import Turtle, Screen
-    t = Turtle()
-    s = Screen()
-    angle = (sides-2)*90/sides
-    for i in range(sides):
-        t.forward(long)
-        t.left(angle)
